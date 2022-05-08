5ba489d704a02d32543abe0d0ee0e7544fff38d6
aksh31 <98390411+aksh31@users.noreply.github.com>
2022-05-08 22:37:31 +0530

    first Push

---
+++ assets/js/app.js
@@ -0,0 +1,41 @@
+let cl = console.log; //=> first class function
+
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=2'; 
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
+
+const data = document.getElementById("data")
+let array = [];
+
+fetch(API_URL)
+    .then(response => response.json())
+    .then(res => {
+        cl(res.results)
+        array = res.results;
+        templating(array);
+    });
+
+
+function templating(arr){
+    let result = '';
+    arr.forEach(ele => {
+        result += `<div class="col-md-3">
+                        <div class="card mb-4">
+                            <div>
+                                <img src="${IMG_PATH+ele.poster_path}" alt="${ele.title}" class="img-fluid myImg">
+                            </div>
+                            <div class="card-body">
+                                <div class="headings">
+                                    <h4 class="text-white">${ele.title}</h4>
+                                    <span class="text-white">${ele.vote_average}</span>
+                                </div>
+                            </div>    
+                            <div class="bg-light p-5 overview">
+                                <h5>Overview:</h5>
+                                ${ele.overview}
+                            </div>
+                        </div>
+                    </div>`
+    });
+    data.innerHTML = result;
+}
