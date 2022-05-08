08b513adf837638c0b6426aa64700c70788f0f5c
aksh31 <98390411+aksh31@users.noreply.github.com>
2022-05-09 01:10:35 +0530

    second push

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,9 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
 
-const data = document.getElementById("data")
+const data = document.getElementById("data");
+const searchBar = document.getElementById("form");
+const search = document.getElementById("search");
 let array = [];
 
 fetch(API_URL)
@@ -19,15 +21,15 @@ fetch(API_URL)
 function templating(arr){
     let result = '';
     arr.forEach(ele => {
-        result += `<div class="col-md-3">
+        result += `<div class="col-md-3 col-sm-6">
                         <div class="card mb-4">
-                            <div>
-                                <img src="${IMG_PATH+ele.poster_path}" alt="${ele.title}" class="img-fluid myImg">
+                            <div class="myImg">
+                                <img src="${IMG_PATH+ele.poster_path}" alt="${ele.title}" class="img-fluid">
                             </div>
                             <div class="card-body">
                                 <div class="headings">
                                     <h4 class="text-white">${ele.title}</h4>
-                                    <span class="text-white">${ele.vote_average}</span>
+                                    <span class="${ratingColor(ele.vote_average)}">${ele.vote_average}</span>
                                 </div>
                             </div>    
                             <div class="bg-light p-5 overview">
@@ -39,3 +41,32 @@ function templating(arr){
     });
     data.innerHTML = result;
 }
+
+
+function ratingColor(rate){
+    if(rate >= 8){
+        return "green";
+    }else if(rate >=4){
+        return "yellow";
+    }else{
+        return "red";
+    }
+}
+
+const onSearchHandler = eve => {
+    eve.preventDefault();
+
+    if(search.value && search.value !== ""){
+        fetch(`${SEARCH_API + search.value}`)
+            .then(response => response.json())
+            .then(res => {
+                cl(res.results)
+                templating(res.results);
+            })
+    searchBar.reset();
+    }else{
+        window.location.reload();
+    }
+}
+
+searchBar.addEventListener("submit", onSearchHandler);
